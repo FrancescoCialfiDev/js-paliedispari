28c659439ce4a1adb6a4be877ce12e2b065288ef
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-11-05 17:02:44 +0100

    fixed issues

--- script/script.js
+++ script/script.js
@@ -3,35 +3,38 @@
 // Console Clear
 console.clear();
 
+// Scegli il gioco
+
+let scegliGioco = prompt("Scegli a quale gioco vuoi giocare: morra oppure palindromo")
+if (scegliGioco === "morra") {
+    palindromo();
+} else if (scegliGioco == "palindromo") {
+    PariODispariGame()
+} else {
+    console.log("Questo gioco non esiste");
+
+}
 
 //______ESERCIZIO_N1_________________________________________
 // Chiediamo all'utente di inserire una parola
-
-//palindromo()
-
 function palindromo() {
+    
+    // Dichiaro le variabili
     let parola = prompt("Inserisci una parola").toLocaleLowerCase();
     let valore = wordReversed(parola);
 
-
-
-
     // Chiamo / Invoco la funzione creata
     wordReversed(parola)
-
-
     // Creaiamo la funzione
-
     function wordReversed(word) {
         let parolaGirata = parola.split('').reverse().join('');
         return parolaGirata;
     }
-
+    // Creiamo la condizione
     if (valore !== parola) {
         console.log("La parola" + " " + parola + " " + "non è palindroma")
     } else {
         console.log("La parola" + " " + parola + " " + "è palindroma");
-
     }
 }
 //_______________________________________________
@@ -39,31 +42,36 @@ function palindromo() {
 //______ESERCIZIO_N2_________________________________________
 
 //PariODispariGame ()
-
-function PariODispariGame () {
+// Chiamo - Invoco la funzione
+function PariODispariGame() {
+    // Dichiaro le variabili 
     let giocatoreNumber = parseInt(prompt("Inserisci un numero da uno a cinque"))
-console.log(giocatoreNumber);
-let pariODispari = prompt("Scegli Pari o Dispari")
-console.log(pariODispari);
-let randomNumberComputer = randomNumber(5, 1);
-console.log(randomNumberComputer);
-let somma = randomNumberComputer + giocatoreNumber;
-let risultatoSomma = calcolo ();
+    console.log(giocatoreNumber);
+    let pariODispari = prompt("Scegli Pari o Dispari")
+    console.log(pariODispari);
+    let randomNumberComputer = randomNumber(5, 1);
+    console.log(randomNumberComputer);
+    let somma = randomNumberComputer + giocatoreNumber;
+    let risultatoSomma = calcolo();
 
-randomNumber(5, 1)
-function randomNumber(max, min) {
-    let numero = Math.floor(Math.random() * (max - min + 1)) + min;
-    return numero;
-}
+    // Creo una funziione per generare un numero randomico e ritorno il valore a randomNumberComputer
+    randomNumber(5, 1)
+    function randomNumber(max, min) {
+        let numero = Math.floor(Math.random() * (max - min + 1)) + min;
+        return numero;
+    }
 
-function calcolo () {
-    somma= somma % 2 === 0
-    return somma;
-}
+    // Creo una funzione per eseguire un calcolo tra il numero che scrive l'user e quello del computer. Ci restituisce valore tru o false
+    function calcolo() {
+        somma = somma % 2 === 0
+        return somma;
+    }
 
-if (risultatoSomma === true && pariODispari === "Pari" ) {
-    console.log("Hai Vinto") 
-} else {
-    console.log("Hai Perso") 
-}
+    // Creo la condizione per stabilire che se il risultato della somma è divisibile per 2 con resto zero, quindi pari mi darà risultato true
+    // Quindi se risultato sarà uguale a true avrai vinto, altrimenti avrai perso.
+    if (risultatoSomma === true && pariODispari === "Pari") {
+        console.log("Hai Vinto")
+    } else {
+        console.log("Hai Perso")
+    }
 }
